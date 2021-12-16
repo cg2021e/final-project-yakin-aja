37550a59d5eb70b7cc5e05b5e869189ea6fce395
auliaaepa <auliaepa@gmail.com>
2021-12-16 15:55:41 +0700

    add truck

--- js/app.js
+++ js/app.js
@@ -45,15 +45,19 @@ dirLight.shadow.camera.top = d;
 dirLight.shadow.camera.bottom = -d;
 
 // define lane property
-const laneTypes = ['car', 'forest'];
+const laneTypes = ['car', 'truck', 'forest'];
 const treeHeights = [20, 45, 60];
 const laneSpeeds = [2, 2.5, 3];
-const carColors = [0x428eff, 0xffef42, 0xff7b42, 0xff426b];
+const vechicleColors = [0x428eff, 0xffef42, 0xff7b42, 0xff426b];
 
 const carFrontTexture = new Texture(40, 80, [{x: 0, y: 10, w: 30, h: 60 }]);
 const carBackTexture = new Texture(40, 80, [{x: 10, y: 10, w: 30, h: 60 }]);
 const carRightSideTexture = new Texture(110, 40, [{x: 10, y: 0, w: 50, h: 30 }, {x: 70, y: 0, w: 30, h: 30 }]);
 const carLeftSideTexture = new Texture(110, 40, [{x: 10, y: 10, w: 50, h: 30 }, {x: 70, y: 10, w: 30, h: 30 }]);
+
+const truckFrontTexture = new Texture(30, 30, [{x: 15, y: 0, w: 10, h: 30 }]);
+const truckRightSideTexture = new Texture(25, 30, [{x: 0, y: 15, w: 10, h: 10 }]);
+const truckLeftSideTexture = new Texture(25, 30, [{x: 0, y: 5, w: 10, h: 10 }]);
 
 // first lanes
 const generateLanes = () => [-9,-8,-7,-6,-5,-4,-3,-2,-1,0,1,2,3,4,5,6,7,8,9].map((index) => {
@@ -63,7 +67,7 @@ const generateLanes = () => [-9,-8,-7,-6,-5,-4,-3,-2,-1,0,1,2,3,4,5,6,7,8,9].map
   return lane;
 }).filter((lane) => lane.index >= 0);
 
-// inisialitation
+// initialization
 const initaliseValues = () => {
   lanes = generateLanes();
 
@@ -117,6 +121,25 @@ function Lane(index) {
       this.mesh.add( vechicle );
       return vechicle;
     });
+
+    this.speed = laneSpeeds[Math.floor(Math.random()*laneSpeeds.length)];
+  } else if (this.type == 'truck') {
+    this.mesh = new Road();
+    this.direction = Math.random() >= 0.5;
+
+    this.occupiedPositions = new Set();
+    this.vechicles = [1,2].map(() => {
+      const vechicle = new Truck();
+      let position;
+      do {
+        position = Math.floor(Math.random()*columns/3);
+      } while (this.occupiedPositions.has(position))
+        this.occupiedPositions.add(position);
+      vechicle.position.x = (position*positionWidth*3+positionWidth/2)*zoom-boardWidth*zoom/2;
+      if(!this.direction) vechicle.rotation.z = Math.PI;
+      this.mesh.add( vechicle );
+      return vechicle;
+    })
 
     this.speed = laneSpeeds[Math.floor(Math.random()*laneSpeeds.length)];
   }
@@ -175,7 +198,7 @@ function Tree() {
   return tree;  
 }
 
-// road 
+// road for vechicle
 function Road() {
   const road = new THREE.Group();
 
@@ -199,7 +222,7 @@ function Road() {
   return road;
 }
 
-// wheel for car
+// wheel for vechicle
 function Wheel() {
   const wheel = new THREE.Mesh( 
     new THREE.BoxBufferGeometry( 12*zoom, 33*zoom, 12*zoom ), 
@@ -209,10 +232,10 @@ function Wheel() {
   return wheel;
 }
 
-// car
+// vechicle: car
 function Car() {
   const car = new THREE.Group();
-  const color = carColors[Math.floor(Math.random() * carColors.length)];
+  const color = vechicleColors[Math.floor(Math.random() * vechicleColors.length)];
   
   const main = new THREE.Mesh(
     new THREE.BoxBufferGeometry( 60*zoom, 30*zoom, 15*zoom ), 
@@ -254,7 +277,61 @@ function Car() {
   return car;  
 }
 
-// texture for car
+// vechicle: truck
+function Truck() {
+  const truck = new THREE.Group();
+  const color = vechicleColors[Math.floor(Math.random() * vechicleColors.length)];
+
+  const base = new THREE.Mesh(
+    new THREE.BoxBufferGeometry( 100*zoom, 25*zoom, 5*zoom ), 
+    new THREE.MeshLambertMaterial( { color: 0xb4c6fc, flatShading: true } )
+  );
+  base.position.z = 10*zoom;
+  truck.add(base)
+
+  const cargo = new THREE.Mesh(
+    new THREE.BoxBufferGeometry( 75*zoom, 35*zoom, 40*zoom ), 
+    new THREE.MeshPhongMaterial( { color: 0xb4c6fc, flatShading: true } )
+  );
+  cargo.position.x = 15*zoom;
+  cargo.position.z = 30*zoom;
+  cargo.castShadow = true;
+  cargo.receiveShadow = true;
+  truck.add(cargo)
+
+  const cabin = new THREE.Mesh(
+    new THREE.BoxBufferGeometry( 25*zoom, 30*zoom, 30*zoom ), 
+    [
+      new THREE.MeshPhongMaterial( { color, flatShading: true } ), // back
+      new THREE.MeshPhongMaterial( { color, flatShading: true, map: truckFrontTexture } ),
+      new THREE.MeshPhongMaterial( { color, flatShading: true, map: truckRightSideTexture } ),
+      new THREE.MeshPhongMaterial( { color, flatShading: true, map: truckLeftSideTexture } ),
+      new THREE.MeshPhongMaterial( { color, flatShading: true } ), // top
+      new THREE.MeshPhongMaterial( { color, flatShading: true } ) // bottom
+    ]
+  );
+  cabin.position.x = -40*zoom;
+  cabin.position.z = 20*zoom;
+  cabin.castShadow = true;
+  cabin.receiveShadow = true;
+  truck.add( cabin );
+
+  const frontWheel = new Wheel();
+  frontWheel.position.x = -38*zoom;
+  truck.add( frontWheel );
+
+  const middleWheel = new Wheel();
+  middleWheel.position.x = -10*zoom;
+  truck.add( middleWheel );
+
+  const backWheel = new Wheel();
+  backWheel.position.x = 30*zoom;
+  truck.add( backWheel );
+
+  return truck;  
+}
+
+// texture for vechicle
 function Texture(width, height, rects) {
   const canvas = document.createElement( "canvas" );
   canvas.width = width;
@@ -289,7 +366,7 @@ function animate(timestamp) {
 
   // cars moving
   lanes.forEach(lane => {
-    if (lane.type === 'car') {
+    if (lane.type === 'car' || lane.type === 'truck') {
       const aBitBeforeTheBeginingOfLane = -boardWidth*zoom/2 - positionWidth*2*zoom;
       const aBitAfterTheEndOFLane = boardWidth*zoom/2 + positionWidth*2*zoom;
       lane.vechicles.forEach(vechicle => {
